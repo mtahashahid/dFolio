7505f343af037a508e9ac938c9b3496785a8cacd
Muhammad Taha <taha_shahid@outlook.com>
2021-06-14 23:19:45 +0500

    navbar modification

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,26 +1,21 @@
 import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
 import Nav from "react-bootstrap/Nav";
-import codeIcon from "../../Assets/code.png";
 import "./Navbar.css";
 class NavBar extends Component {
   render() {
     return (
       <div>
         <Navbar collapseOnSelect expand="lg" className="nav-bg" variant="dark">
-          {/* <img
-            alt=""
-            src={codeIcon}
-            width="30"
-            height="30"
-            // className="d-inline-block align-top"
-          /> */}
           <Navbar.Brand href="#home" className="name">
             Muhammad Taha
           </Navbar.Brand>
-          <Navbar.Toggle aria-controls="responsive-navbar-nav" variant="dark" />
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="ml-auto">
+            <Nav
+              className="ml-auto text-center"
+              style={{ fontFamily: "Varela Round,sans-serif" }}
+            >
               <Nav.Link href="#skills" className="mr-4 pl-4 text-secondary h5">
                 Skills
               </Nav.Link>
