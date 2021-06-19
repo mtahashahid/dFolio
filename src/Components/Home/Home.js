2d27dcb6da4fcb8f711e4a54e640706e86c96000
Muhammad Taha <taha_shahid@outlook.com>
2021-06-19 23:36:07 +0500

    working on home component

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import './Home.css'
+import HomeImg from "../../../src/Assets/programmer.svg";
+class Home extends Component {
+  render() {
+    return (
+      <div>
+       <section className=" text-dark p-5 text-center">
+        <div className="container">
+          <div className="d-sm-flex">
+            <div>
+              <h1>Hi! I am <span className="text-primary">Muhammad Taha</span></h1>
+              <p className="lead">I a Computer Science Student | Web developer </p>
+            </div>
+            <img className="img-fluid w-50" src={HomeImg} alt="taha"></img>
+          </div>
+        </div>
+       </section>
+      </div>
+    );
+  }
+}
+export default Home;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import NavBar from '../Components/Navbar/NavBar'
+import Home from '../Components/Home/Home'
 class Layout extends Component {
     render() {
         return (
             <div>
                <NavBar/>
+               <Home/>
             </div>
         )
     }
