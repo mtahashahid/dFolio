6fe64ba4682fee607c0a938aacee7ffcb2368ff1
Muhammad Taha <taha_shahid@outlook.com>
2021-06-23 23:56:08 +0500

    navbar developed

--- src/Components/Navbar/NavBar.js
+++ src/Components/Navbar/NavBar.js
@@ -1,34 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-class NavBar extends Component {
-  handleClick(){
-    
-  }
-  render() {
-    return (
-      <nav>
-        <label className="name">Muhammad Taha</label>
-        <ul>
-          <li>
-            <a className="active" href="!#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="!#">Skills</a>
-          </li>
-          <li>
-            <a href="!#">Projects</a>
-          </li>
-          <li>
-            <a href="!#">Contact</a>
-          </li>
-        </ul>
-        <label className="icon">
-          <i className="fa fa-bars" aria-hidden="true" onClick={this.handleClick}></i>
-        </label>
-      </nav>
-    );
-  }
-}
+
+const NavBar = () => {
+  const [navbarOpen,setNavbarOpen] = useState(false);
+  const handleToggle = () => {
+    setNavbarOpen(prev => !prev)
+  };
+  return (
+    <nav>
+      <label className="name">Muhammad Taha</label>
+      <ul>
+        <li>
+          <a className="active" href="!#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="!#">Skills</a>
+        </li>
+        <li>
+          <a href="!#">Projects</a>
+        </li>
+        <li>
+          <a href="!#">Contact</a>
+        </li>
+      </ul>
+      <label className="icon">
+        <i className="fa fa-bars" aria-hidden="true" onClick={handleToggle}>{`${navbarOpen ? "show" : ""}`}</i>
+      </label>
+    </nav>
+  );
+};
 export default NavBar;
