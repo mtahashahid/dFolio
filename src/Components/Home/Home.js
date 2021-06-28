7499f642e629f78c1931dc0250d6c636863b62a1
Muhammad Taha <taha_shahid@outlook.com>
2021-06-28 16:19:41 +0500

    home section changes

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,14 +4,21 @@ import HomeImg from "../../../src/Assets/programmer1.svg";
 class Home extends Component {
   render() {
     return (
-      <div className="home-wrapper">
-       <div className="home-right">
-         <h1>Hi there,I'm Muhammad Taha</h1>
-             
-       </div>
-       <div className="home-left">   
-           <img src={HomeImg} alt="home"></img>
-       </div>
+      <div className="home-main">
+        <div className="content-left">
+          <h1>Hi there,I'm Muhammad Taha</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique
+            quas eum, velit enim officia quae, saepe possimus hic perspiciatis
+          </p>
+          <div className="home-btns">
+          <button className="btn">Contact Me</button>
+          <button className="btn btn-2">Contact Me</button>
+          </div>
+        </div>
+        <div className="content-right">
+          <img src={HomeImg} alt="home" className="home-img"></img>
+        </div>
       </div>
     );
   }
